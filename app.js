26ab4977ad021c476ef493243c78b6eb0e7caf6f
Stevenson Nelson <sonson521@gmail.com>
2020-11-29 15:09:07 -0500

    add minus and add event.

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
-const btn = document.querySelector('#click');
+const addCount = document.querySelector('#addCount');
+
+const lowerCount = document.querySelector('#lowerCount');
 
 const num = document.querySelector('#number');
 
-let count = 1;
+let count = 0;
 
-btn.addEventListener('click', function () {
+addCount.addEventListener('click', add);
+
+function add() {
   num.innerText = count++;
-});
+  console.log(count);
+}
+
+lowerCount.addEventListener('click', minus);
+
+function minus() {
+  num.innerText = count--;
+  console.log(count);
+}
