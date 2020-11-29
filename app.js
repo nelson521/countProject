c73a9cfe77d50af78fbb7ebdaa04287267051343
Stevenson Nelson <sonson521@gmail.com>
2020-11-28 21:10:44 -0500

    add event listener

---
+++ app.js
@@ -0,0 +1,9 @@
+const btn = document.querySelector('#click');
+
+const num = document.querySelector('#number');
+
+let count = 0;
+
+btn.addEventListener('click', function () {
+  console.log('clicked');
+});
